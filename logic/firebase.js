2050fd49e28a750818cc0ade185b1dc2f6495136
minirafa1799 <105514668+minirafa1799@users.noreply.github.com>
2022-08-27 10:37:17 -0300

    upload

---
+++ logic/firebase.js
@@ -0,0 +1,22 @@
+
+ // insira suas credenciais aqui
+ const firebaseConfig = {
+
+    apiKey: "AIzaSyAwXP774eSd9B-vYhdleOaRO3esiYt1Qdw",
+  
+    authDomain: "to-do-list-minirafa.firebaseapp.com",
+  
+    databaseURL: "https://to-do-list-minirafa-default-rtdb.firebaseio.com",
+  
+    projectId: "to-do-list-minirafa",
+  
+    storageBucket: "to-do-list-minirafa.appspot.com",
+  
+    messagingSenderId: "501418367083",
+  
+    appId: "1:501418367083:web:6530355557700721d6b92e"
+  
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
